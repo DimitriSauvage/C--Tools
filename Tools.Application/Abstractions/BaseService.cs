be92efe0e043140dbc3d20775dacef5589f0d08f
SAUVAGE Dimitri <dimitri1993@live.fr>
2020-04-17 11:52:54 +0200

    Ajout du service comme base de l'API Controller

--- Tools.Application/Abstractions/BaseService.cs
+++ Tools.Application/Abstractions/BaseService.cs
@@ -2,6 +2,7 @@
 using AutoMapper;
 using Microsoft.Extensions.DependencyInjection;
 using Tools.Domain.Abstractions;
+using Tools.Helpers;
 using Tools.Infrastructure.Abstraction;
 
 namespace Tools.Application.Abstractions
@@ -9,28 +10,18 @@ namespace Tools.Application.Abstractions
     public abstract class BaseService<TEntity, TRepository> where TEntity : class, IEntity
         where TRepository : IRepository<TEntity>
     {
+        #region Fields
+
         /// <summary>
         /// Data manager
         /// </summary>
-        protected TRepository Repository { get; }
+        protected TRepository Repository { get; } =
+            ServiceCollectionHelper.GetElementFromDependencyInjection<TRepository>();
 
         /// <summary>
         /// Mapper
         /// </summary>
-        protected IMapper Mapper { get; }
-
-        #region Constructors
-
-        protected BaseService()
-        {
-            //Get the service collection
-            IServiceCollection serviceCollection = new ServiceCollection();
-            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
-
-            //Get the values
-            this.Repository = serviceProvider.GetService<TRepository>();
-            this.Mapper = serviceProvider.GetService<IMapper>();
-        }
+        protected IMapper Mapper { get; } = ServiceCollectionHelper.GetElementFromDependencyInjection<IMapper>();
 
         #endregion
     }
--- Tools.Mvc/Abstractions/ApiController.cs
+++ Tools.Mvc/Abstractions/ApiController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Tools.Application.Abstractions;
+using Tools.Domain.Abstractions;
+using Tools.Helpers;
+using Tools.Infrastructure.Abstraction;
 
 namespace Tools.Mvc.Abstractions
 {
@@ -7,7 +13,21 @@ namespace Tools.Mvc.Abstractions
     [Authorize]
     [Produces("application/json")]
     [Route("api")]
-    public abstract class ApiController : BaseController
+    public abstract class ApiController<TEntity, TService> : BaseController
+        where TEntity : class, IEntity
+        where TService : BaseService<TEntity, IRepository<TEntity>>
     {
+        #region Fields
+
+        /// <summary>
+        /// Data manager
+        /// </summary>
+        protected TService Service { get; } = ServiceCollectionHelper.GetElementFromDependencyInjection<TService>();
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
     }
 }
