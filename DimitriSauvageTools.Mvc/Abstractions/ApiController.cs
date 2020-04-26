2df1036089e3ab6382a6087393d59e23cc5b5d64
SAUVAGE Dimitri <dimitri1993@live.fr>
2020-04-26 18:44:29 +0200

    Modification de RouteAttribute vers AreaAttibute sur le base ApiController

--- DimitriSauvageTools.Mvc/Abstractions/ApiController.cs
+++ DimitriSauvageTools.Mvc/Abstractions/ApiController.cs
@@ -27,7 +27,7 @@ namespace DimitriSauvageTools.Mvc.Abstractions
     [ApiController]
     [Authorize]
     [Produces("application/json")]
-    [Route("api")]
+    [Area("api")]
     public abstract class ApiController : ControllerBase
     {
     }
